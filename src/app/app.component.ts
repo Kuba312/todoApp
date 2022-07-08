0b8d61127787fb65c40802a82aff6b8f2736f504
Jakub <jakub.michalak98@wp.pl>
2022-07-08 13:54:08 +0200

    todo-app-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Task} from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-udemy';
+  // taskName: string;
+  editMode = false;
+  taskName = 'Siłownia';
+  taskDate = '';
+  config: { [key: string]: string } = null;
+
+  tasks: Task[] = [{
+    name: 'Siłownia',
+    deadLine: '2020-06-12',
+    done: false
+  },
+    {
+      name: 'Nauka Angulara',
+      deadLine: '2020-10-06',
+      done: false
+    },
+  ];
+
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: 'Lista Zadań zbudowanych w Angularze',
+        date: new Date().toDateString(),
+      };
+    }, 500);
+    this.sortTasks();
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  // onKeyUp(key: KeyboardEvent) {
+  //   const target = key.target as HTMLInputElement;
+  //   this.taskName = target.value;
+  // }
+
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      deadLine: this.taskDate,
+      done: false
+    };
+    this.tasks.push(task);
+    this.taskName = '';
+    this.taskDate = '';
+    this.sortTasks();
+    // Add to interface tasks
+  }
+
+  switchEditMode() {
+    this.editMode = !this.editMode;
+  }
+
+  markTaskAsDone(task: Task) {
+    task.done = true;
+    this.sortTasks();
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter(e => e !== task);
+    this.sortTasks();
+  }
+
+  private sortTasks() {
+    this.tasks = this.tasks.sort((a: Task, b: Task) =>
+      a.done === b.done ? 0 : a.done ? 1 : -1
+    );
+  }
+
+
 }
---
+++ src/app/task.ts
@@ -0,0 +1,5 @@
+export interface Task {
+  name: string; // The name of task to do
+  deadLine: string; // Object which said when task will be finish
+  done: boolean; // If task will be done or not
+}
